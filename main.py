3b3f18aefc51bfd4a3529cd452dd6e72b36f3e93
kunal15145 <kunal15145@iiitd.ac.in>
2018-04-28 01:33:34 +0530

    reading code added

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import read as read
 
-read.readdata(3)
+data = read.readdata(3)
+print(data.keys())
+print(len(data[0]))
+print(len(data[1]))
+print(data[0][0].shape)
--- read.py
+++ read.py
@@ -1,4 +1,5 @@
 import glob as glob
+import cv2
 
 
 def readdata(filtersize):
@@ -16,3 +17,29 @@ def readdata(filtersize):
 
     # 0 class non medianfilterd 1 class median filtered
     data_dict = {0: [], 1: []}
+
+    for imagepath in glob.glob(path + "*"):
+        image = cv2.imread(imagepath, 0)
+        m, n = image.shape
+        imagen = image.reshape((1, m * n))
+        data_dict[0].append(imagen)
+
+    for imagepath in glob.glob(path1 + "*"):
+        image = cv2.imread(imagepath, 0)
+        m, n = image.shape
+        imagen = image.reshape((1, m * n))
+        data_dict[0].append(imagen)
+
+    for imagepath in glob.glob(path2 + "*"):
+        image = cv2.imread(imagepath, 0)
+        m, n = image.shape
+        imagen = image.reshape((1, m * n))
+        data_dict[1].append(imagen)
+
+    for imagepath in glob.glob(path3 + "*"):
+        image = cv2.imread(imagepath, 0)
+        m, n = image.shape
+        imagen = image.reshape((1, m * n))
+        data_dict[1].append(imagen)
+
+    return data_dict
