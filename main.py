96af03eba1a313c100cf02a9d12d02d8ffc4c3a2
kunal15145 <kunal15145@iiitd.ac.in>
2018-04-28 12:00:53 +0530

    train test split and mfr window

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import read as read
 
 data = read.readdata(3)
-print(data.keys())
-print(len(data[0]))
-print(len(data[1]))
-print(data[0][0].shape)
+data = read.preprecessing_MFR(data)
+splitted_data = read.data_split(data)
--- read.py
+++ read.py
@@ -1,8 +1,11 @@
 import glob as glob
 import cv2
+import scipy.ndimage as scn
+from sklearn.model_selection import train_test_split
 
 
 def readdata(filtersize):
+    global n, m
     path = "dataset/jpeg45/"
     path1 = "dataset/jpeg90/"
     path2 = ""
@@ -42,4 +45,34 @@ def readdata(filtersize):
         imagen = image.reshape((1, m * n))
         data_dict[1].append(imagen)
 
-    return data_dict
+    return [data_dict, m, n]
+
+
+def data_split(data):
+
+    images = data[0]
+    fulldata = []
+    fulllabels = []
+    for classvalue,im in images.items():
+        for i in range(len(im)):
+            fulldata.append(im[i])
+            fulllabels.append(classvalue)
+
+    X_train, X_test, y_train, y_test = train_test_split(fulldata, fulllabels, test_size=0.30, random_state=42)
+    return [X_train, X_test, y_train, y_test]
+
+
+def preprecessing_MFR(data):
+    size_m = data[1]
+    size_n = data[2]
+    images = data[0]
+
+    for classvalue, imagesdata in images.items():
+        for im in range(len(imagesdata)):
+            finalimage = imagesdata[im].reshape((size_m, size_n))
+            denoised = scn.median_filter(finalimage, 3)
+            mfrimage = denoised - finalimage
+            mfrimage = mfrimage.reshape((1, size_m * size_n))
+            imagesdata[im] = mfrimage
+
+    return [images, size_m, size_n]
